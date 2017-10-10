391c5191fb1a13196d63a282a5ddc1280c8326aa
Yifan Wu <tyivanwu@gmail.com>
2017-10-10 13:47:11 -0400

    delete default soundtrack & clear full playlist

--- src/store/store.js
+++ src/store/store.js
@@ -6,12 +6,7 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     strict: true,
     state: {
-        playlist: [{
-            author: "Zombie Chang",
-            pic: "http://img.xiami.net/images/album/img56/1277136656/21002723041454654560.jpg",
-            title: "ARE YOU READY",
-            url: "http://om5.alicdn.com/656/1277136656/2100272304/1775601398_59686758_l.mp3?auth_key=cc4d10b77498d82b173fde8c087eb3b1-1506567600-0-null"
-        }],
+        playlist: [],
         load: true
     },
     mutations: {
@@ -25,7 +20,7 @@ export const store = new Vuex.Store({
             state.load = !state.load;
         },
         clearPlaylist: (state) => {
-            state.playlist.splice(1);
+            state.playlist.splice(0);
         }
     },
     actions: {
